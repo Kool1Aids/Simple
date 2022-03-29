05564b92afe0cd34bb302c5741137f14f8c8bc0e
Kool1Aids <carral.ervin@live.jobcorps.org>
2022-03-29 16:11:41 +0000

    bob

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var myVar;
 var myVara;
 var gameVar;
+var restart;
+var home;
 const player = document.getElementById('player');
+const obj = document.getElementById('obj')
 
 function myFunction() {
     myVar = setTimeout(showPage, 1000);
@@ -27,6 +30,25 @@ function showGame() {
     document.getElementById('continueBtn').style.display = 'none';
 }
 
+function loser() {
+    document.getElementById('button').style.display = 'block';
+    document.getElementById('retry').style.display = 'none';
+}
+
+function soreLoser() {
+    home = setTimeout(loser(), 1000);
+}
+
+function startOver() {
+    document.getElementById('game').style.display = 'block';
+    document.getElementById('retry').style.display = 'none';
+    document.getElementById('score').innerHTML = 0;
+}
+
+function restartGame() {
+    restart = setTimeout(startOver(), 1000);
+}
+
 function jump() {
     if (player.classList != 'jump') {
     player.classList.add('jump');
@@ -35,8 +57,32 @@ function jump() {
         player.classList.remove('jump')
     }, 300)
 }
+} 
+
+let isAlive = setInterval(function() {
+    // players Y pos
+    let playerTop = parseInt(window.getComputedStyle(player).getPropertyValue('top'));
+    // obj X pos
+    let objLeft = parseInt(window.getComputedStyle(obj).getPropertyValue('left'));
+    //colision
+    if(objLeft < 50 && objLeft > 0 && playerTop >= 300) {
+        document.getElementById('retry').style.display = 'block'
+        document.getElementById('game').style.display = 'none'
+    }
+
+    if(objLeft === 0) {
+        points()
+    }
+}, 10)
+
+function points() {
+    var score = parseInt(document.getElementById('score').innerHTML);
+    score++;
+    document.getElementById('score').innerHTML = score;
+    document.getElementById('points').innerHTML = score;
 }
 
 document.addEventListener("keydown", function(event) {
     jump()
-})
+}) 
+
