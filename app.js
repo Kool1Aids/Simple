569f5644f68f7651e90ce554119f3adcb6ab1855
Kool1Aids <carral.ervin@live.jobcorps.org>
2022-03-29 13:03:29 +0000

    bob

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 var myVar;
-var myVara
+var myVara;
+var gameVar;
+const player = document.getElementById('player');
 
 function myFunction() {
     myVar = setTimeout(showPage, 1000);
+    myVara = setTimeout(showButton, 5000);
+}
+
+function startGame() {
+    gameVar = setTimeout(showGame(), 1000);
 }
 
 function showPage() {
@@ -10,4 +17,26 @@ function showPage() {
     document.getElementById('loadingScreen').style.display = 'block';
 }
 
+function showButton() {
+    document.getElementById('continueBtn').style.display = 'block';
+}
 
+function showGame() {
+    document.getElementById('game').style.display = 'block';
+    document.getElementById('loadingScreen').style.display = 'none';
+    document.getElementById('continueBtn').style.display = 'none';
+}
+
+function jump() {
+    if (player.classList != 'jump') {
+    player.classList.add('jump');
+
+    setTimeout(function() {
+        player.classList.remove('jump')
+    }, 300)
+}
+}
+
+document.addEventListener("keydown", function(event) {
+    jump()
+})
